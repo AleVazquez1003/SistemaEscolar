7ce9caa7238bfba2ce1d0db09f2d63ae3ecd5e41
MiguelVazquez <0vazquez.miguel.1j@gmail.com>
2025-03-26 14:46:24 -0600

    Prueba

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 //Enrutado de componentes para sus vistas
 export const routes: Routes = 
 [
+    //Prueba 
     //paths de las rutas ejemplo
     //{path: 'carpeta', component: Component}
 ];
