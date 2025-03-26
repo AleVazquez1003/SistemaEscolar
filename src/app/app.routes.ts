fbd5327bff4ff9baa563b5f7275bad512ca09536
MiguelVazquez <0vazquez.miguel.1j@gmail.com>
2025-03-26 15:31:44 -0600

    Añade ruta de home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
 //Enrutado de componentes para sus vistas
 export const routes: Routes = 
 [
     //Prueba 
+    {path: 'home', component: HomeComponent}
 ];
