c1be413e40a6d78d0b6af47815bd6ce8ad5eed6e
AleVazquez <0Vazquez.miguel.1j@gmail.com>
2025-03-25 18:14:53 -0600

    Añade comentarios de ejemplo para path de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 
 //Enrutado de componentes para sus vistas
-export const routes: Routes = [];
+export const routes: Routes = 
+[
+    //paths de las rutas ejemplo
+    //{path: 'carpeta', component: Component}
+];
