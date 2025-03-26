5ac2773fe8c37a8590a05c4a52d27f3ef6f9e227
MiguelVazquez <0vazquez.miguel.1j@gmail.com>
2025-03-26 14:41:58 -0600

    Merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,4 @@ import { Routes } from '@angular/router';
 //Enrutado de componentes para sus vistas
 export const routes: Routes = 
 [
-    //paths de las rutas ejemplo
-    //{path: 'carpeta', component: Component}
 ];
