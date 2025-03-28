b6246ee923c77200228dc17e0b1822f83bfa0eb7
MiguelVazquez <0vazquez.miguel.1j@gmail.com>
2025-03-27 22:25:09 -0600

    Diseño de home path de rutas dani ramirez y creacion de components professor, qualification y attendance

---
+++ src/app/Components/Professor/professor-update/professor-update.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professor-update',
+  imports: [],
+  templateUrl: './professor-update.component.html',
+  styleUrl: './professor-update.component.css'
+})
+export class ProfessorUpdateComponent {
+
+}
---
+++ src/app/Components/Qualification/qualification-list/qualification-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qualification-list',
+  imports: [],
+  templateUrl: './qualification-list.component.html',
+  styleUrl: './qualification-list.component.css'
+})
+export class QualificationListComponent {
+
+}
